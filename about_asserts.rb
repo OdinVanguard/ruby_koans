722a65efa2bbcdcb84bee3cf12af3c391c4a2dda
Wesley Botello-Smith <wesleymsmith2000@yahoo.com>
2016-01-02 16:07:36 -0800

    Finished about_asserts koans, started about_nil

--- about_asserts.rb
+++ about_asserts.rb
@@ -3,38 +3,39 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
+
 class AboutAsserts < Neo::Koan
 
-  # We shall contemplate truth by testing reality, via asserts.
-  def test_assert_truth
-    assert true #false                # This should be true
-  end
+	# We shall contemplate truth by testing reality, via asserts.
+	def test_assert_truth
+		assert true #false # This should be true
+	end
 
-  # Enlightenment may be more easily achieved with appropriate
-  # messages.
-  def test_assert_with_message
-    assert true, "This is now correctly set to true" #false, "This should be true -- Please fix this"
-  end
+	# Enlightenment may be more easily achieved with appropriate
+	# messages.
+	def test_assert_with_message
+		assert true, "This is now correctly set to true" #false, "This should be true -- Please fix this"
+	end
 
-  # To understand reality, we must compare our expectations against
-  # reality.
-  def test_assert_equality
-    expected_value = __
-    actual_value = 1 + 1
+	# To understand reality, we must compare our expectations against
+	# reality.
+	def test_assert_equality
+		expected_value = 2
+		actual_value = 1 + 1
 
-    assert expected_value == actual_value
-  end
+		assert expected_value == actual_value
+	end
 
-  # Some ways of asserting equality are better than others.
-  def test_a_better_way_of_asserting_equality
-    expected_value = __
-    actual_value = 1 + 1
+	# Some ways of asserting equality are better than others.
+	def test_a_better_way_of_asserting_equality
+		expected_value = 2
+		actual_value = 1 + 1
 
-    assert_equal expected_value, actual_value
-  end
+		assert_equal expected_value, actual_value
+	end
 
-  # Sometimes we will ask you to fill in the values
-  def test_fill_in_values
-    assert_equal __, 1 + 1
-  end
+	# Sometimes we will ask you to fill in the values
+	def test_fill_in_values
+		assert_equal 2, 1 + 1
+	end
 end
