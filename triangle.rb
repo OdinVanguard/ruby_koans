4f4833b7f3c8e61547565f8773eb890edd87aa66
Wesley Botello-Smith <wesleymsmith2000@yahoo.com>
2016-01-05 17:20:57 -0800

    finished about_triangle_project_2.rb

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,20 @@
 #
 
 def triangle(a, b, c)
+	if (a <= 0 || b <= 0 || c <= 0) 
+		raise TriangleError,"All lengths must be positive"
+	end
+	
+	args=[a,b,c].sort
+
+	unless ( args[0]+args[1] > args[2] )
+		raise TriangleError,"Longest length may not be greater than the sum of the other lengths"
+	end
+
+	if (args[0]-args[1] > args[2])
+		raise TriangleError,"Longest length must be greater than the difference of the other two lengths"
+	end
+
 	triangletypes=[:scalene,:isosceles,:equilateral]
 	if (a==b || b==c || a==c)
 		if (a==b && a==c && b==c)
@@ -23,6 +37,7 @@ def triangle(a, b, c)
 		return triangletypes[1]
 	end
 	return triangletypes[0]
+	
 end
 
 # Error class used in part 2.  No need to change this code.
